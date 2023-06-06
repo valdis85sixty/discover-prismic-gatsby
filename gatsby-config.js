fe1ae493fa863be5c506087a0fc91e780d671930
Valdis Locs <valdis.locs@digitaloperative.com>
2023-06-06 16:54:19 +0300

    Add Prismic Plugins, Add Github Action

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,14 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-prismic`,
+      options: {
+        repositoryName: `${process.env.PRISMIC_REPO_NAME}`,
+        customTypesApiToken: `${process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN}`,
+      },
+    },
+  ],
 }
